8397e94c12761564ce8b6e665a22da9a31e9ab83
tushark-16bit <kalra.tushar.16@gmail.com>
2024-12-17 16:49:06 +0530

    Initial Commit

---
+++ src/main/java/com/healthcare/healthcare_parent/HealthcareParentApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.healthcare_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareParentApplication.class, args);
+	}
+
+}
